04ebe84f31053dd9ef45e75009c32c8f9076ba1f
Jake Kopulsky <jakekopulsky@gmail.com>
2018-04-19 12:43:41 -0700

    Add enable cors to express server

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const db = require('../database/database.js');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 
 app.use(express.static('../dist'));
