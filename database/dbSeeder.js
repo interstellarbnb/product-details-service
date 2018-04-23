0dc828983fffaac013555f5683ddc249144f2bd2
Jake Kopulsky <jakekopulsky@gmail.com>
2018-04-22 22:08:20 -0700

    Update dbSeed and schema

--- database/dbSeeder.js
+++ database/dbSeeder.js
@@ -1,14 +1,41 @@
 const db = require('./database.js');
-// Plugin for generating fake data
+const _ = require('underscore');
 const faker = require('faker');
-
+/*
+    Seed generator will pick a random # of each field to generate seed data
+*/
 const types = ['Station', 'Wormhole', 'Alpha Base', 'Moon', 'Satellite',
   'Black Hole', 'Rover', 'Crater', 'Asteroid', 'Rocket Ship'];
 
+const basicAmenities = ['Wifi 3.0', 'Interstellar TV', 'Geiger counter', 'Washer', 'Dryer',
+  'Central heating', 'Air conditioning', 'Shuttle charging', 'First aid kit', 'Carbon monoxide monitor',
+  'Siri enabled', 'Wadsworth - personal robotic butler', 'Flux sensor', 'Haptic drive'];
+
+const diningAmenities = ['Coffe maker', 'Basic cooking utensils', 'Dishes and silverware', 'Microwave', 'Oven',
+  'Stove', 'Refrigerator', 'Full bar', 'Mini bar', 'Geothermal oven', 'Food sanitizer', 'Wonka vision'];
+
+const bedBathAmenities = ['Sleeping pods', 'Smartflow toilets', 'Self-cleaning mattress', 'floating beds',
+  'Chromatherapy shower', 'Auto towel-warming', 'Smart mirror', 'Bathtub', 'Heated floors'];
+
+const facilitiesAmenities = ['Shuttle dock', 'Hot tub', 'Swimming pool', 'Bubble shield', 'Solar powered',
+  'Organic farm', 'Simulation station', 'Moisture vaporator', 'Cogwave jammer', 'Geomapper module', 'Tesla coil'];
+
+// Returns an array composed of a random selection of a random # of elements from a given list
+const generateAmenities = (arr) => {
+  const randNum = Math.floor(1 + (Math.random() * arr.length));
+  return _.sample(arr, randNum);
+};
+
+const generateNotIncluded = (basics) => {
+  console.log('in generator');
+  console.log(basics);
+  return _.difference(basicAmenities, basics);
+};
+
 let typeCount = 0;
-for (let i = 1; i < 100; i += 1) {
+for (let i = 1; i < 3; i += 1) {
   const randNum = Math.floor(1 + (Math.random() * 6));
-  const oneSeed = {
+  const newSeed = {
     id: i,
     spaceType: types[typeCount],
     spaceTitle: faker.commerce.productName(),
@@ -17,22 +44,29 @@ for (let i = 1; i < 100; i += 1) {
     numBedrooms: randNum,
     numBeds: randNum,
     numBaths: randNum,
-    host: { 
+    host: {
       name: faker.name.findName(),
       pictureUrl: faker.image.avatar(),
     },
     summary: {
-      plot:  faker.lorem.paragraph(),
+      plot: faker.lorem.paragraph(),
       space: faker.lorem.paragraph(),
       notes: faker.lorem.paragraph(),
       interactionWithGuest: faker.lorem.paragraph(),
     },
+    amenities: {
+      basics: generateAmenities(basicAmenities),
+      dining: generateAmenities(diningAmenities),
+      bedBath: generateAmenities(bedBathAmenities),
+      facilitiesAmenities: generateAmenities(facilitiesAmenities),
+      notIncluded: (() => { return (_.difference(basicAmenities, this.newSeed.amenities.basics)) })(),
+    },
   };
-  db.Listing.create(oneSeed, (err, small) => {
-    if (err) return console.log(err);
-    //
-  });
-
+  // db.Listing.create(newSeed, (err, small) => {
+  //   if (err) return console.log(err);
+  //   //  I should update this to use promies
+  // });
+  console.log(newSeed.amenities);
   if (typeCount === 9) {
     typeCount = 0;
   }
